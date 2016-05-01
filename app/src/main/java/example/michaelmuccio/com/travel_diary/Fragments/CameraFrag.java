f1b4c4198801469dbc1166531d32788cc2d34cfd
Michael Muccio <michaelmuccio@gmail.com>
2016-05-01 11:12:55 -0700

    camera class initialized, Camera Frag Init, camera Layout init

--- app/src/main/java/example/michaelmuccio/com/travel_diary/Fragments/CameraFrag.java
+++ app/src/main/java/example/michaelmuccio/com/travel_diary/Fragments/CameraFrag.java
@@ -1,7 +1,52 @@
 package example.michaelmuccio.com.travel_diary.Fragments;
 
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+
+import example.michaelmuccio.com.travel_diary.R;
+import example.michaelmuccio.com.travel_diary.Services.CameraView;
+
 /**
  * Created by michaelmuccio on 5/1/16.
  */
-public class CameraFrag {
+public class CameraFrag extends Fragment{
+    private Camera mCamera = null;
+    private CameraView mCameraView = null;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.camera_frag, container, false);
+
+        try{
+            mCamera = Camera.open();//you can use open(int) to use different cameras
+        } catch (Exception e){
+            Log.d("ERROR", "Failed to get camera: " + e.getMessage());
+        }
+
+        if(mCamera != null) {
+            mCameraView = new CameraView(this, mCamera);//create a SurfaceView to show camera data
+            FrameLayout camera_view = (FrameLayout) findViewById(R.id.camera_view);
+            camera_view.addView(mCameraView);//add the SurfaceView to the layout
+        }
+
+        //btn to close the application
+        ImageButton imgClose = (ImageButton) findViewById(R.id.imgClose);
+        imgClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.exit(0);
+            }
+        });
+
+        return v;
+    }
 }
