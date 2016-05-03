d296b7873c0f449a952d90273ff8f59d19417ff0
Michael Muccio <michaelmuccio@gmail.com>
2016-05-02 18:19:22 -0700

    created switch statement for home screen, cam, and following views

--- app/src/main/java/example/michaelmuccio/com/travel_diary/Activities/MainActivity.java
+++ app/src/main/java/example/michaelmuccio/com/travel_diary/Activities/MainActivity.java
@@ -1,75 +1,90 @@
 package example.michaelmuccio.com.travel_diary.Activities;
 
 import android.graphics.Color;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
 
+import example.michaelmuccio.com.travel_diary.Fragments.AddTripFrag;
+import example.michaelmuccio.com.travel_diary.Fragments.CameraFrag;
 import example.michaelmuccio.com.travel_diary.Fragments.FeedFragment;
 import example.michaelmuccio.com.travel_diary.R;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
     AHBottomNavigation bottomNavigation;
+    AHBottomNavigationItem item1;
+    AHBottomNavigationItem item2;
+    AHBottomNavigationItem item3;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-        //noinspection ConstantConditions
-//        getSupportActionBar().setTitle("Bottom Navigation");
-
         bottomNavInit();
-       // navigationSelected();
-
+        navigationSelected();
     }
 
-    public void bottomNavInit(){
+    public void bottomNavInit() {
         bottomNavigation = (AHBottomNavigation) findViewById(R.id.bottom_navigation);
 
-        AHBottomNavigationItem item1 = new AHBottomNavigationItem("Home", R.drawable.ic_home_black_24dp);
-        AHBottomNavigationItem item2 = new AHBottomNavigationItem("Camera", R.drawable.ic_camera_alt_black_24dp);
-        AHBottomNavigationItem item3 = new AHBottomNavigationItem("Contacts", R.drawable.ic_person_black_24dp);
+        item1 = new AHBottomNavigationItem("Home", R.drawable.ic_home_black_24dp);
+        item2 = new AHBottomNavigationItem("Camera", R.drawable.ic_camera_alt_black_24dp);
+        item3 = new AHBottomNavigationItem("Following", R.drawable.ic_person_black_24dp);
 
         bottomNavigation.addItem(item1);
         bottomNavigation.addItem(item2);
         bottomNavigation.addItem(item3);
 
-        bottomNavigation.setDefaultBackgroundColor(R.color.colorPrimary);
+        item1.setColor(R.color.colorPrimary);
         bottomNavigation.setAccentColor(R.color.colorAccent);
         bottomNavigation.setInactiveColor(R.color.colorPrimaryDark);
+        bottomNavigation.setColored(true);
 
     }
 
-//    public void navigationSelected(){
-//        final FeedFragment fragment = new FeedFragment();
-//        Bundle bundle = new Bundle();
-//        bundle.putInt("color", Color.parseColor(colors[0]));
-//        fragment.setArguments(bundle);
-//
-//        getSupportFragmentManager()
-//                .beginTransaction()
-//                .add(R.id.frame, fragment, "square")
-//                .commit();
-//        //  Enables Reveal effect
-//        bottomNavigation.setColored(true);
-//
-//        bottomNavigation.setCurrentItem(0);
-//
-//        bottomNavigation.setOnTabSelectedListener(new AHBottomNavigation.OnTabSelectedListener() {
-//            @Override
-//            public void onTabSelected(int position, boolean wasSelected) {
-//                // Do something cool here...
-//
-//                fragment.updateColor(Color.parseColor(colors[position]));
-//            }
-//        });
-//    }
+    public void navigationSelected() {
+        bottomNavigation.setOnTabSelectedListener(new AHBottomNavigation.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(int position, boolean wasSelected) {
+                // Do something cool here..
+                AddTripFrag addTripFrag = new AddTripFrag();
+                CameraFrag cameraFrag = new CameraFrag();
+                FeedFragment feedFragment =  new FeedFragment();
+                fragmentManager =  getSupportFragmentManager();
+
+                switch (position) {
+                    case 0:
+                        fragmentTransaction = fragmentManager.beginTransaction();
+                        fragmentTransaction.replace(R.id.frag_container, feedFragment);
+                        fragmentTransaction.commit();
+                       // toolbar.setTitle("Feed");
+                        break;
+                    case 1:
+                        fragmentTransaction = fragmentManager.beginTransaction();
+                        fragmentTransaction.replace(R.id.frag_container, cameraFrag);
+                        fragmentTransaction.commit();
+                        //toolbar.setTitle("Add a Trip!");
+                        break;
+                    case 2:
+                        fragmentTransaction = fragmentManager.beginTransaction();
+                        fragmentTransaction.replace(R.id.frag_container, addTripFrag);
+                        fragmentTransaction.commit();
+                       // toolbar.setTitle("Following");
+                        break;
+                }
+            }
+        });
+    }
 
 
 }
