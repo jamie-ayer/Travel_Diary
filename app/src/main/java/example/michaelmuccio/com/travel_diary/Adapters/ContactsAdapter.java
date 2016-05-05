4d8d2c277c2ea0d0defd6716a436e57aca79b64b
Michael Muccio <michaelmuccio@gmail.com>
2016-05-05 15:51:33 -0700

    completed adapter

--- app/src/main/java/example/michaelmuccio/com/travel_diary/Adapters/ContactsAdapter.java
+++ app/src/main/java/example/michaelmuccio/com/travel_diary/Adapters/ContactsAdapter.java
@@ -1,29 +1,38 @@
 package example.michaelmuccio.com.travel_diary.Adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.TextView;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import example.michaelmuccio.com.travel_diary.R;
 
 /**
  * Created by michaelmuccio on 5/4/16.
  */
-public class ContactsAdapter extends RecyclerView.Adapter<> {
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ContactsListViewHolder> {
+    private static OnItemClickListener listener;
+    Context context;
+    LinkedList list;
 
     public ContactsAdapter() {
 
     }
 
-    public interface onitemClickListener{
+    public interface OnItemClickListener{
         void onItemClick(View itemView, int position);
     }
 
-    public void setOnItemClickListener(AdapterView.OnItemClickListener listener) {
+    public void setOnItemClickListener(OnItemClickListener listener) {
         this.listener = listener;
     }
 
@@ -49,4 +58,23 @@ public class ContactsAdapter extends RecyclerView.Adapter<> {
         }
     }
 
+    @Override
+    public ContactsListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.users_info_view_for_adapter, parent, false);
+        ContactsListViewHolder vh = new ContactsAdapter.ContactsListViewHolder(view);
+
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ContactsListViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
 }
